a0f337f89208988829d19245ce25e268b6269db7
Dmytro Hapchyn <48380610+Sseik@users.noreply.github.com>
2026-02-05 18:35:22 +0200

    Add keyboard navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 const statusDiv = document.querySelector(".status");
+const addBookButton = document.querySelector(".add-button");
+const submitBookButton = document.querySelector(".submit-book");
+const newBookModal = document.querySelector(".new-book-modal");
+
+function toggleNewBookModalDisplay() {
+  newBookModal.classList.toggle("not-displayed");
+}
 
 statusDiv.addEventListener("click", () => statusDiv.classList.toggle("read"));
+addBookButton.addEventListener("click", toggleNewBookModalDisplay);
+submitBookButton.addEventListener("click", (e) => {
+  toggleNewBookModalDisplay();
+});
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "Escape":
+      newBookModal.classList.add("not-displayed");
+      break;
+    case "Enter":
+      if (!newBookModal.classList.contains("not-displayed")) {
+        submitBookButton.click();
+      }
+      break;
+    case " ":
+      document.activeElement.click();
+  }
+});
